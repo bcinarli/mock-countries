b400c7c9fb7afe5da48d7086bceb50d9883eb3ad
Bilal Çınarlı <bcinarli@gmail.com>
2017-12-19 10:47:48 +0100

    cors :)

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 3000);
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/list', (req, res) => {
     res.status(200).send(countries);
 });
